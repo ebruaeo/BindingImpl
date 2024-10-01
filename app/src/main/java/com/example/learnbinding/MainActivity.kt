65d18df82265564fed8ba255e35c931c0f233030
ebruaeo <ebruaeo@hotmail.com>
2024-10-01 22:33:02 +0300

    View Binding logic

---
+++ app/src/main/java/com/example/learnbinding/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.learnbinding
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.learnbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+   // private lateinit var textView : TextView
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Clicked button "
+        }
+        // textView = findViewById(R.id.textView)
+    }
+
+}
